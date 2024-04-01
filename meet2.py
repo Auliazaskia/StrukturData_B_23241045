7899b0c25dba648cccdd6f1132875762985670d4
Auliazaskia <146051775+Auliazaskia@users.noreply.github.com>
2024-04-01 12:15:16 +0800

    1 APRIL 2024

---
+++ meet2.py
@@ -0,0 +1,20 @@
+# belajar variabel
+#assigment 
+x = 3
+y = 9
+uang = 1000
+
+#pemanggilan 
+print ("Nilai UAS saya =", x)
+print ("Nilai UTS saya =", y)
+print ("sisa uang saya =", uang)
+
+# penamaan variabel
+nilai_a = 9 # tidak boleh di spasi
+kotak10 = 10 # tidak boleh diawali angka
+
+#input varabel kedua
+x = 30
+print ("Nilai UAS saya=", x)
+X = 30
+print ("Nilai UAS saya=", x)
