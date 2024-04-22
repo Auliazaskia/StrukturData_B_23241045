db56f673fcef14da50db7167891626676af8b339
Auliazaskia <146051775+Auliazaskia@users.noreply.github.com>
2024-04-22 11:54:29 +0800

    Kuliah 22 April 2024

---
+++ AuliaZaskia/Folder Baru/Folder Baru/Auliazaskia/exe3.py
@@ -0,0 +1,21 @@
+# contoh tipe data (integar) : angka satuan 
+bil_integer = 10001
+print(type(bil_integer))
+print ("type data dari bil_integer adalah :",type(bil_integer))
+
+# contoh tipe data (float) : bilangan desimal 
+bil_float = 2.5
+print ("type data dari bil_integer adalah :",type(bil_float))
+
+# contoh tipe data (string) : kumpulan karalter  
+bil_string = "2.5"
+print ("type data dari bil_integer adalah :",type(bil_string))
+
+# contoh tipe data (bolean) : kumpulan biner, 0,1, True False 
+bil_bol = False 
+print ("type data dari bil_integer adalah :",type(bil_bol))
+
+# tipe data dari bahasa c
+from ctypes import c_double
+bil_double = c_double(20000000)
+print ("type data bil_double adalah: ", type(bil_double))
---
+++ AuliaZaskia/Folder Baru/Folder Baru/Auliazaskia/meet3.py
@@ -0,0 +1,5 @@
+# contoh tipe data (integar) : angka satuan 
+bil_integer = 1
+print(type(bil_integer))
+
+type data dari bil_integer adalah int
